3124374a9dc593a4df659ee6f8f4185c70f04458
tiffany <tiffanymchu@gmail.com>
2024-07-16 13:34:41 -0700

    test

--- main1.py
+++ main1.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(
     page_title="Natural Language Processing App",
@@ -7,9 +8,18 @@ st.set_page_config(
     
 )
 
-def home_page():
-    st.title("This is the Home Page.")
-    st.write("Welcome to the Natural Language Processing App!")
-    st.write("Use the sidebar to navigate to different sections of the app.")
+st.title("This is the Home Page.")
+st.write("Welcome to the Natural Language Processing App!")
+st.write("Use the sidebar to navigate to different sections of the app.")
 
-home_page()
+
+st.write("Upload a CSV file")
+
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.session_state['uploaded_file'] = df  #save the uploaded file to session state
+    st.write("File uploaded successfully! You can now navigate to the NLP tools on the sidebar to work with this file.")
+    st.write("Uploaded CSV file:")
+    st.write(df.head())
