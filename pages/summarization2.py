550f1dd7161491c0bd2b149f2c76def1d7e5acdb
tiffany <tiffanymchu@gmail.com>
2024-08-07 11:16:17 -0700

    update

--- pages/summarization2.py
+++ pages/summarization2.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import torch
 from transformers import PegasusForConditionalGeneration, AutoTokenizer
+from transformers import PegasusTokenizer
 
 model_name = 'google/pegasus-cnn_dailymail'
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
